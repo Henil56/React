2ac88a33c20fa5d4752ba16c5669f97629eccd43
Henil56 <henilkukadiya56@gmail.com>
2026-01-25 01:54:38 +0530

    comp compete

--- 12MegaBlog/src/components/index.js
+++ 12MegaBlog/src/components/index.js
@@ -4,11 +4,21 @@ import Footer from "./footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./header/LogoutBtn"
-
+import RTE from "./RTE";
+import  Signup from "./Signup"
+import Login from "./Login"
+import Post from "./PostCard"
+import PostForm from "./post-form/PostForm"
 export{
     Header,
     Footer,
     Container,
     Logo,
-    LogoutBtn
+    LogoutBtn,
+    RTE,
+    Signup,
+    Login,
+    Post,
+    PostForm,
+    
 }
